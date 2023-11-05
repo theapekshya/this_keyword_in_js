4bf2ddeb44c6ee15cf56bd85f3c6d2596d97e3cc
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-05 22:43:34 +0545

    this_keyword_in_js

---
+++ this.js
@@ -0,0 +1,14 @@
+// this keyword in js
+
+let person = {
+    firstname : "Apekshya",
+    sayHello()
+    {
+        console.log("Hello! I am " + this.firstname + " and I have a " + car.brand + " car.");
+    }
+    };
+let car = {
+    brand : "Iconic",
+    model : "safari"
+};
+person.sayHello();
